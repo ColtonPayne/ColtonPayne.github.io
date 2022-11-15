f49d86f7a476df1b49f8e6c79ae115015691534e
ColtonPayne <72282946+ColtonPayne@users.noreply.github.com>
2022-11-15 11:18:32 -0700

    Try

--- scripts/loadTokens.js
+++ scripts/loadTokens.js
@@ -1,29 +1,3 @@
-function getHTTPGateway(ipfsLink){
-    var prefix = 'https';
-    var uri = ipfsLink.substring(4, ipfsLink.length - 5);
-    var suffix = '.ipfs.nftstorage.link/blob';
-    return prefix + uri + suffix;
-}
-
-// Create a div element for each token you own
-function getTokens(){
-    for (let i = 1; i < 3; i++) {
-        const img = document.createElement("img");
-        var ipfsLink = tokens[i].image;
-        img.src = getHTTPGateway(ipfsLink);
-        img.height = 200;
-        img.width = 200;
-        document.getElementById("nfts").appendChild(img);
-        const TID = document.createElement("h2");
-        TID.innerHTML = tokens[i].tokenId;
-        document.getElementById("nfts").appendChild(TID);
-        const md = document.createElement("h2");
-        md.innerHTML = tokens[i].metadata.attributes[1].value;
-        document.getElementById("nfts").appendChild(md);
-    }
-}
-
-
 window.addEventListener('m-authenticated', async (event) => {
     // Get the data client instance
     const client = event.detail.client;
@@ -35,9 +9,24 @@ window.addEventListener('m-authenticated', async (event) => {
     const h2 = document.createElement("h2");
     h2.innerHTML = "Your NFTs";
     document.getElementById("nfts").appendChild(document.createElement("h2"))
-    
-    getTokens()
-
+    // Create a div element for each token you own
+    for (let i = 0; i < tokens.length; i++) {
+        const img = document.createElement("img");
+        var ipfsLink = tokens[i].image;
+        var prefix = 'https';
+        var uri = ipfsLink.substring(4, ipfsLink.length - 5);
+        var suffix = '.ipfs.nftstorage.link/blob';
+        img.src = prefix + uri + suffix;
+        img.height = 200;
+        img.width = 200;
+        document.getElementById("nfts").appendChild(img);
+        const TID = document.createElement("h2");
+        TID.innerHTML = tokens[i].tokenId;
+        document.getElementById("nfts").appendChild(TID);
+        const md = document.createElement("h2");
+        md.innerHTML = tokens[i].metadata.attributes[1].value;
+        document.getElementById("nfts").appendChild(md);
+    }
 })
 window.addEventListener('m-unauthenticated', async (event) => {
     document.getElementById("nfts").innerHTML = "";
