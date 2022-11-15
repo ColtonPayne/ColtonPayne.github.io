60e09185379c2c1477ccd4c8b460ad15d5e8d613
ColtonPayne <72282946+ColtonPayne@users.noreply.github.com>
2022-11-15 11:40:59 -0700

    Add hardhat dir

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-ethers');
+
+const ALCHEMY_API_KEY = "ALCHEMY_API_KEY";
+const GOERLI_PRIVATE_KEY = "PRIVATE_KEY"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+      gas: 10000000
+    }
+  }
+};
